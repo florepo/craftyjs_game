fe7fc14d5b313f0dc55a5f1a82344fb604cb7deb
Marianne Benkamoun <marianne.benkamoun@gmail.com>
2019-04-10 19:12:21 +0100

    initial setup of the game

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var GAME_SCREEN_WIDTH = 800;
-var GAME_SCREEN_HEIGHT = 600;
+var GAME_SCREEN_WIDTH = 600;
+var GAME_SCREEN_HEIGHT = 400;
 
 Crafty.init(GAME_SCREEN_WIDTH,GAME_SCREEN_HEIGHT, document.getElementById('game'));
 
-Crafty.scene("Loading");
+Crafty.scene("Game");
--- scenes/game.js
+++ scenes/game.js
@@ -1,7 +1,13 @@
 Crafty.defineScene("Game", function() {
-	
-	var redSquare = Crafty.e('2D, DOM, Color')
-		.attr({x: 0, y: 0, w: 100, h: 100})
-		.color('#F00');
+
+	var redSquare = Crafty.e("2D, Canvas, Color, Gravity")
+  .attr({ x: 200, y: 50, w: 50, h: 50 })
+  .color("black")
+  .gravity("Floor");
+
+  var floor = Crafty.e("2D, Canvas, Color, Twoway, Floor")
+  .attr({ x: 200, y: 340, w: 200, h: 50 })
+  .color("orange")
+  .twoway(300);
 
 });
