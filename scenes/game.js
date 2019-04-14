ad11c1e14c1d90e718a5bb32ef4deb5a2475ab2d
Sumia <sumia.017@gmail.com>
2019-04-14 16:21:58 +0100

    Added points

--- scenes/game.js
+++ scenes/game.js
@@ -1,8 +1,21 @@
 Crafty.defineScene("Game", function() {
 	
+	var total_points = 0;
 
+	//label: score
+	Crafty.e('2D, DOM, Text') 
+		.attr({w: 50, h: 100, x: 650, y: 20})
+		.text("Score:")
+		
+	//points container
+	var points = Crafty.e('2D, DOM, Text') 
+		.attr({w: 50, h: 100, x: 700, y: 20})
+		.text("0")
+		.bind("UpdateScore", function(score){
+			this.text(score)
+		});
 
-
+	//the payer
 	var redSquare = Crafty.e('2D, DOM, Color, Twoway, Gravity, Collision, player')
 		.attr({x: 0, y: 0, w: 100, h: 100})
 		.color('#F00')
@@ -11,7 +24,12 @@ Crafty.defineScene("Game", function() {
 		.onHit('coin', function(hitDatas){
 			Crafty.log("hit the object");
 			 for (var i = 0, l = hitDatas.length; i < l; ++i) { // for each bullet hit
-          		hitDatas[i].obj.destroy(); // destroy the bullet
+				//hide obj
+				hitDatas[i].obj.destroy(); // destroy the bullet
+
+				//update point
+				total_points += 5;
+				Crafty.trigger("UpdateScore", total_points);
 			 }
 		});
 
