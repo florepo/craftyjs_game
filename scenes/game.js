ff139eaaa3ee78ed88fa164d98ba02c017586c31
Gio <dallavecchiacareer@gmail.com>
2019-04-14 17:25:00 +0100

    Added Coins

--- scenes/game.js
+++ scenes/game.js
@@ -1,7 +1,7 @@
 Crafty.defineScene("Game", function () {
 
 	var redSquare = Crafty.e('2D, DOM, Color, Twoway, Gravity')
-		.attr({ x: 0, y: 0, w: 70, h: 70 })
+		.attr({ x: 0, y: 0, w: 50, h: 50 })
 		.color('#F00')
 		.twoway(200, 300)
 		.gravity('Floor');
@@ -15,16 +15,15 @@ Crafty.defineScene("Game", function () {
 		.attr({ x: 556, y: 590, w: 160, h: 10 })
 		.color('yellow');
 
-	// ADD COINS (SQUARE)
-	var initialX = 180;
-	var posY = 420;
-	
-	
-	var yellowSquare = Crafty.e('2D, DOM, Color')
-		.attr({ x: 180, y: 420, w: 70, h: 70 })
-		.color('yellow');
-	var yellowSquare = Crafty.e('2D, DOM, Color')
-		.attr({ x: 320, y: 420, w: 70, h: 70 })
-		.color('yellow');
-	
+	// CREATE A FOR LOOP TO ADD THE COINS
+	var posY = 480;
+	var posX = 70;
+
+	for(var i = 0; i < 5; i++) {
+		Crafty.e('2D, Collision, Canvas, Color, coin')
+			.attr({x: posX, y: posY, w: 30, h: 30})
+			.color('yellow')
+		posX += 140;
+	}
+
 });
