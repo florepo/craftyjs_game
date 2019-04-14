05edb6751bf6609f75af78377074e1f75b67fefd
Gio <dallavecchiacareer@gmail.com>
2019-04-14 22:01:16 +0100

    Score board

--- scenes/game.js
+++ scenes/game.js
@@ -1,10 +1,34 @@
 Crafty.defineScene("Game", function () {
 
-	var redSquare = Crafty.e('2D, DOM, Color, Twoway, Gravity')
+	var score = 0;
+
+	// LABEL SCORE
+	Crafty.e("2D, DOM, Text")
+		.attr({ w: 30, h: 100, x: 700, y: 20 })
+		.text("Score")
+		.textFont({ size: '10px', weight: 'bold' });
+
+	Crafty.e("2D, DOM, Text")
+		.attr({ w: 20, h: 100, x: 750, y: 20 })
+		.text("0")
+		.textFont({ size: '10px', weight: 'bold' })
+		.bind("updateScore", function(score){
+			this.text(score)
+		});
+	// PLAYER
+	var redSquare = Crafty.e('2D, DOM, Color, Twoway, Gravity, Collision')
 		.attr({ x: 0, y: 0, w: 40, h: 40 })
 		.color('#F00')
 		.twoway(200, 300)
-		.gravity('Floor');
+		.gravity('Floor')
+		.onHit('Coin', function(hitDatas){
+			 for (var i = 0, l = hitDatas.length; i < l; ++i) { // WHEN COLLIDE AGAINST COIN HIDE IT
+				hitDatas[i].obj.destroy();
+				// UPDATE THE SCORE
+				score ++;
+				Crafty.trigger("updateScore", score);
+			 }
+		})
 
 	// PLATFORMS
 	var green_floor = Crafty.e('Floor, 2D, Canvas, Color')
@@ -20,13 +44,11 @@ Crafty.defineScene("Game", function () {
 	// COINS
 	var posY = 480;
 	var posX = 70;
-	var changeColor = False
 
-	for(var i = 0; i < 5; i++) {
-		Crafty.e('2D, Collision, Canvas, Color, coin')
-			.attr({x: posX, y: posY, w: 30, h: 30})
+	for (var i = 0; i < 5; i++) {
+		Crafty.e('2D, Collision, Canvas, Color, Coin')
+			.attr({ x: posX, y: posY, w: 30, h: 30 })
 			.color('yellow')
-			.collision()
 		posX += 140;
 	}
 
