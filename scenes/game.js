49b74b8a83a65cc9739759e07ae48816a8963689
Sumia <sumia.017@gmail.com>
2019-04-14 16:30:35 +0100

    added comments

--- scenes/game.js
+++ scenes/game.js
@@ -33,53 +33,41 @@ Crafty.defineScene("Game", function() {
 			 }
 		});
 
+	//platform1
 	var obs1 = Crafty.e('Floor, 2D, Canvas, Color')
 		.attr({x: 0, y: 550, w: 130, h: 5})
 		.color('green')
 
+	//platform2
 	var obs2 = Crafty.e('Floor, 2D, Canvas, Color')
 		.attr({x: 300, y: 550, w: 130, h: 5})
 		.color('blue')
 
+	//platform3
 	var obs3 = Crafty.e('Floor, 2D, Canvas, Color')
 		.attr({x: 600, y: 550, w: 130, h: 5})
 		.color('orange')
 
-		var mx = 150;
-		var my = 350;
-		for(var i = 0; i < 3; i++) {
-			Crafty.e('2D, Collision, Canvas, Color, coin')
-				.attr({x: mx, y: my, w: 10, h: 10})
-				.color('yellow')
-			mx+= 30;
-			my-=10;
-		}
+	//region coins
+	var mx = 150;
+	var my = 350;
+	for(var i = 0; i < 3; i++) {
+		Crafty.e('2D, Collision, Canvas, Color, coin')
+			.attr({x: mx, y: my, w: 10, h: 10})
+			.color('yellow')
+		mx+= 30;
+		my-=10;
+	}
 
-		
-		for(var i = 0; i < 3; i++) {
-			Crafty.e('2D, Collision, Canvas, Color, coin')
-				.attr({x: mx, y: my, w: 10, h: 10})
-				.color('yellow')
-			mx+= 30;
-			my+=10;
-		}
 	
-		// Crafty.e('2D, Collision, Canvas, Color, coin')
-		// // .checkHits('Solid')
-		// .attr({x: 180, y: 280, w: 10, h: 10})
-		// .color('yellow')
-		
-		// .bind("HitOn", function(hitData) {
-       	// 	this.destroy() // fire the HitOn event in the next frame also, if the collision is still active.
-    	// })
-		// .onHit('bullet', function(hitDatas) {
-
-		// })	
-
-	// Crafty.e("2D, Collision")
-	// 	.checkHits('coin')
-	// 	.bind("HitOn", function(hitData) {
-
-	// 	});
+	for(var i = 0; i < 3; i++) {
+		Crafty.e('2D, Collision, Canvas, Color, coin')
+			.attr({x: mx, y: my, w: 10, h: 10})
+			.color('yellow')
+		mx+= 30;
+		my+=10;
+	}
+	//endregion coins
+	
 
 });
